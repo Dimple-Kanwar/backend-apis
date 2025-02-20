bdd45dbca7a2511afb13e1a34d2c9aa8ec6b095d
RomarioKavin1 <romario7kavin@gmail.com>
2025-02-20 11:18:26 +0530

    gitignore

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,30 +5,30 @@ import "dotenv/config";
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
-    "base_sepolia": {
+    base_sepolia: {
       url: process.env.BASE_TESTNET_RPC,
-      accounts: [process.env.ADMIN_ACCOUNT_PK!]
+      accounts: [process.env.ADMIN_ACCOUNT_PK!],
     },
-    "eth_sepolia": {
-      url: process.env.SEPOLIA_TESTNET_RPC,
-      accounts: [process.env.ADMIN_ACCOUNT_PK!]
-    },
-    "arbitrum_sepolia": {
+    // "eth_sepolia": {
+    //   url: process.env.SEPOLIA_TESTNET_RPC,
+    //   accounts: [process.env.ADMIN_ACCOUNT_PK!]
+    // },
+    arbitrum_sepolia: {
       url: process.env.ARBITRUM_TESTNET_RPC,
-      accounts: [process.env.ADMIN_ACCOUNT_PK!]
+      accounts: [process.env.ADMIN_ACCOUNT_PK!],
     },
-    "gnosis_chiado_testnet": {
+    gnosis_chiado_testnet: {
       url: process.env.GNOSIS_TESTNET_RPC,
-      accounts: [process.env.ADMIN_ACCOUNT_PK!]
+      accounts: [process.env.ADMIN_ACCOUNT_PK!],
     },
     rootstock_testnet: {
       url: process.env.ROOTSTOCK_TESTNET_RPC,
-      accounts: [process.env.ADMIN_ACCOUNT_PK!]
+      accounts: [process.env.ADMIN_ACCOUNT_PK!],
     },
     localhost: {
-      url: "http://127.0.0.1:8545/"
-    }
-  }
+      url: "http://127.0.0.1:8545/",
+    },
+  },
 };
 
 export default config;
